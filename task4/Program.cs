7a01d0e9e6fad03ce495d59ebcf6e04cbe0f9c58
Ilya <sannikov444@yandex.ru>
2022-11-28 12:33:09 +0300

    дополнительная задача

---
+++ task4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введете число кустов: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int sum=0;
+
+for(int i=1;i<=a;i++){
+    Console.Write($"Введете количество ягод на {i} кусте: ");
+    sum+=Convert.ToInt32(Console.ReadLine());
+}
+Console.Write($"Количество ягод на всех кустах : {sum}");
