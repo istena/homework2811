c23a7753a810f6178417b7888734886b36aa2747
Ilya <sannikov444@yandex.ru>
2022-11-28 11:56:05 +0300

    Add task19

---
+++ task1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введете любое пятизначное число:");
+int a = Convert.ToInt32(Console.ReadLine());
+if((a%10)==(a/10000)){
+    if(((a%100)/10)==((a/1000)%10)){
+        Console.Write("Да");
+    }
+}
+else 
+    Console.Write("Нет");
