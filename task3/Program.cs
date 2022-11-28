79d7ebf6ce280238213c2bb0ec736ee1f463ebff
Ilya <sannikov444@yandex.ru>
2022-11-28 12:21:35 +0300

    Add task23

---
+++ task3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введете любое  число:");
+int a = Convert.ToInt32(Console.ReadLine());
+int i=1;
+while(i<a){
+    Console.Write($"{i*i*i}, ");
+    i++;
+}
+Console.Write(a*a*a);
