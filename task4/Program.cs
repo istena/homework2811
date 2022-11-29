5d70c31e909da078329d1d855dc705d21e1bb208
Ilya <sannikov444@yandex.ru>
2022-11-29 15:36:54 +0300

    дополнительная задача

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,10 +1,17 @@
 ﻿Console.Clear();
 Console.Write("Введете число кустов: ");
 int a = Convert.ToInt32(Console.ReadLine());
-int sum=0;
+int [] b = new int [a];
+int []sum=new int[a-2];
 
 for(int i=1;i<=a;i++){
     Console.Write($"Введете количество ягод на {i} кусте: ");
-    sum+=Convert.ToInt32(Console.ReadLine());
+    b[i-1]=Convert.ToInt32(Console.ReadLine());
 }
-Console.Write($"Количество ягод на всех кустах : {sum}");
+int res=0;
+for (int j=1;j<=(a-2);j++){
+    if((b[j-1]+b[j]+b[j+1])>res)
+        res=b[j-1]+b[j]+b[j+1];
+
+}
+Console.Write($"Максимальное количество трех рядостоящих куста =  : {res}");
